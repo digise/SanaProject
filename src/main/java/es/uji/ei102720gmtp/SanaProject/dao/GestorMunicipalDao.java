66ca172966fc2d2ca6b42fb64bca1e78515dbb14
Ainhoa <al386133@uji.es>
2021-04-13 12:52:14 +0200

    class GestorMunicipalDao y GestorMunicipalRowMapper

---
+++ src/main/java/es/uji/ei102720gmtp/SanaProject/dao/GestorMunicipalDao.java
@@ -0,0 +1,70 @@
+package es.uji.ei102720gmtp.SanaProject.dao;
+
+import es.uji.ei102720gmtp.SanaProject.model.GestorMunicipal;
+import es.uji.ei102720gmtp.SanaProject.model.Municipi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorMunicipalDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    /* Afegim el gestor municipal */
+    public void addGestorMunicipal(GestorMunicipal gestorMunicipal) {
+        jdbcTemplate.update("INSERT INTO GestorMunicipal VALUES(?, ?, ?, ?, ?, ?, ?)",
+                gestorMunicipal.getNif(), gestorMunicipal.getIdMunicipi(), gestorMunicipal.getNom(), gestorMunicipal.getCognoms(), gestorMunicipal.getEmail(), gestorMunicipal.getTelefon(), gestorMunicipal.getContrasenya());
+    }
+
+    /* Esborrem el gestor amb el idMunicipi*/
+    public void deleteGestor(String id) {
+        jdbcTemplate.update("DELETE FROM GestorMunicipal WHERE id_municipi=?",
+                id);
+    }
+
+    /* Esborrem el gestor */
+    public void deleteGestor(GestorMunicipal gestorMunicipal) {
+        jdbcTemplate.update("DELETE FROM GestorMunicipal WHERE nif=?",
+                gestorMunicipal.getNif());
+    }
+
+    /* Actualitzem els atributs del gestor
+       (excepte el nif, que és la clau primària) */
+    public void updateGestor(GestorMunicipal gestorMunicipal) {
+        jdbcTemplate.update("UPDATE GestorMunicipal SET id_municipi = ?, nom = ?, cognoms = ?, email = ?, telefon = ?, contrasenya = ? WHERE nif = ?",
+                gestorMunicipal.getIdMunicipi(), gestorMunicipal.getNom(), gestorMunicipal.getCognoms(), gestorMunicipal.getEmail(), gestorMunicipal.getTelefon(),gestorMunicipal.getContrasenya(), gestorMunicipal.getNif());
+    }
+
+    /* Obtenim el gestor amb el nif. Torna null si no existeix. */
+    public GestorMunicipal getGestor(String nif) {
+        try {
+            return jdbcTemplate.queryForObject(
+                    "SELECT * FROM GestorMunicipal WHERE nif = ?",
+                    new GestorMunicipalRowMapper(),
+                    nif);
+        }
+        catch(EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    /* Obtenim tots els gestors. Torna una llista buida si no n'hi ha cap. */
+    public List<GestorMunicipal> getGestors() {
+        try {
+            return jdbcTemplate.query(
+                    "SELECT * FROM GestorMunicipal",
+                    new GestorMunicipalRowMapper());
+        } catch (EmptyResultDataAccessException e) {
+            return new ArrayList<GestorMunicipal>();
+        }
+    }
+}
---
+++ src/main/java/es/uji/ei102720gmtp/SanaProject/dao/GestorMunicipalRowMapper.java
@@ -0,0 +1,24 @@
+package es.uji.ei102720gmtp.SanaProject.dao;
+
+import es.uji.ei102720gmtp.SanaProject.model.GestorMunicipal;
+import es.uji.ei102720gmtp.SanaProject.model.Municipi;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public final class GestorMunicipalRowMapper implements RowMapper<GestorMunicipal> {
+
+    public GestorMunicipal mapRow(ResultSet rs, int rowNum) throws SQLException
+    {
+        GestorMunicipal gestorMunicipal = new GestorMunicipal();
+        gestorMunicipal.setNif(rs.getString("nif"));
+        gestorMunicipal.setIdMunicipi(rs.getString("id_municipi"));
+        gestorMunicipal.setNom(rs.getString("nom"));
+        gestorMunicipal.setCognoms(rs.getString("cognoms"));
+        gestorMunicipal.setEmail(rs.getString("email"));
+        gestorMunicipal.setTelefon(rs.getString("telefon"));
+        gestorMunicipal.setContrasenya(rs.getString("contrasenya"));
+        return gestorMunicipal;
+    }
+}
