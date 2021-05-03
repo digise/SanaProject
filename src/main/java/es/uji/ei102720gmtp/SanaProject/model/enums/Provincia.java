e993109c6fabf589d92ec7e384f455d232728417
Ainhoa <al386133@uji.es>
2021-05-03 19:37:51 +0200

    Solucionado problemas con el enum Provincia

--- src/main/java/es/uji/ei102720gmtp/SanaProject/model/enums/Provincia.java
+++ src/main/java/es/uji/ei102720gmtp/SanaProject/model/enums/Provincia.java
@@ -1,13 +1,7 @@
 package es.uji.ei102720gmtp.SanaProject.model.enums;
 
 public enum Provincia {
-    VALENCIA("Valencia"),
-    CASTELLO("Castello"),
-    ALACANT("Alacant");
-
-    private String nom;
-
-    Provincia(String nom) {
-        this.nom = nom;
-    }
+    VALENCIA,
+    CASTELLO,
+    ALACANT
 }
