9a01742344c99009cf236d110727b42aef45c98d
joan_padilla <joan@speedycontrol.com>
2021-06-12 17:36:25 +0200

    Nova versio de creacio de codiQR

--- src/main/java/es/uji/ei102720gmtp/SanaProject/controller/ReservaController.java
+++ src/main/java/es/uji/ei102720gmtp/SanaProject/controller/ReservaController.java
@@ -3,6 +3,7 @@ package es.uji.ei102720gmtp.SanaProject.controller;
 import com.google.zxing.*;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import es.uji.ei102720gmtp.SanaProject.Validation.ReservaValidator;
 import es.uji.ei102720gmtp.SanaProject.dao.EspaiPublicDao;
@@ -22,7 +23,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpSession;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -129,24 +133,21 @@ public class ReservaController {
         reservaSimple.setEstat(reserva.getEstat());
         reservaSimple.setNifCiutada(reserva.getNifCiutada());
 
+        File f = new File("imagenes/reserva" + reserva.getIdEspai() + reserva.getIdFranja()
+                + reserva.getIdZona() + reserva.getDataReserva().toString() + ".png");
         String data = "localhost:8080/reserva/" + reserva.getIdEspai() + reserva.getIdFranja()
                 + reserva.getIdZona() + reserva.getDataReserva().toString();
-        String path = "imagenes/reserva" + reserva.getIdEspai() + reserva.getIdFranja()
-                + reserva.getIdZona() + reserva.getDataReserva().toString() + ".png";
-        String charset= "UTF-8";
-        Map<EncodeHintType, ErrorCorrectionLevel> hashMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
-        hashMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+
         try {
-            createQR(data, path, charset, hashMap, 200, 200);
-            reservaSimple.setCodiQr(path);
+            createQR(f, data, 300, 300);
+            System.out.println("Codi QR creat");
         } catch (Exception e){
             System.out.println("No s'ha pogut crear el codi QR");
-            path = "";
-            reservaSimple.setCodiQr(path);
         }
+        reservaSimple.setCodiQr(f.getPath());
 
         reservaDao.addReserva(reservaSimple);
-        Reserva reservaGuardada = reservaDao.getReservaFromQR(path);
+        Reserva reservaGuardada = reservaDao.getReservaFromQR(f.getPath());
 
         Ocupa ocupa = new Ocupa();
         ocupa.setDataReserva(reserva.getDataReserva());
@@ -158,16 +159,30 @@ public class ReservaController {
         return "redirect:/espaiPublic/seleccionarProvincia";
     }
 
-    private static void createQR(String data, String path, String charset, Map hashMap, int height, int width)
-            throws WriterException, IOException, NotFoundException {
-        BitMatrix matrix = new MultiFormatWriter().encode(
-                new String(data.getBytes(charset), charset),
-                BarcodeFormat.QR_CODE, width, height);
+    private File createQR(File file, String data, int height, int width)
+            throws Exception {
+        QRCodeWriter writer = new QRCodeWriter();
+        BitMatrix matrix = writer.encode(data, com.google.zxing.BarcodeFormat.QR_CODE, width, height);
 
-        MatrixToImageWriter.writeToFile(
-                matrix,
-                path.substring(path.lastIndexOf('.') + 1),
-                new File(path));
+        BufferedImage image = new BufferedImage(matrix.getWidth(), matrix.getHeight(), BufferedImage.TYPE_INT_RGB);
+        image.createGraphics();
+
+        Graphics2D graphics = (Graphics2D) image.getGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, matrix.getWidth(), matrix.getHeight());
+        graphics.setColor(Color.BLACK);
+
+        for (int i = 0; i < matrix.getWidth(); i++) {
+            for (int j = 0; j < matrix.getHeight(); j++) {
+                if (matrix.get(i, j)) {
+                    graphics.fillRect(i, j, 1, 1);
+                }
+            }
+        }
+
+        ImageIO.write(image, "png", file);
+
+        return file;
     }
 
     @RequestMapping(value="/update/{id}", method = RequestMethod.GET)
